03f12316b91a078928992bfcab18063d26826b8a
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-06-25 23:54:13 +0530

    Learned a bit of array in js

---
+++ Day11/index.js
@@ -0,0 +1,12 @@
+let arr = [1,2,3,4,5];
+console.log(arr)
+console.log(arr.length)
+console.log(arr[0])
+arr[0]= 1234
+arr[5]= 789
+console.log(arr)
+console.log(arr.toString())
+console.log(arr.join(" and "))
+console.log(arr.pop())
+console.log(arr.push("HEY"))
+console.log(arr)
