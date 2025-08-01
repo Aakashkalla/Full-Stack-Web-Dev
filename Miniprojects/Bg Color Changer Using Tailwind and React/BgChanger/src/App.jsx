91229632c93042d59a9c755fca7097aa57013137
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-08-01 19:41:26 +0530

    Built a Bg Changer Using React + Tailwind CSS

---
+++ Miniprojects/Bg Color Changer Using Tailwind and React/BgChanger/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+function App() {
+  const [color, setColor] = useState("lightblue");
+
+  return (
+    <div className="w-full min-h-screen duration-200" style={{ backgroundColor: color }}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl'>
+          <button onClick={() => setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"red"}}>Red</button>
+          <button onClick={() => setColor("Cyan")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor:"Cyan"}}>Cyan</button>
+          <button onClick={() => setColor("Brown")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"Brown"}}>Brown</button>
+          <button onClick={() => setColor("Violet")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"Violet"}}>Violet</button>
+          <button onClick={() => setColor("Orange")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"Orange"}}>Orange</button>
+          <button onClick={() => setColor("Pink")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"Pink"}}>Pink</button>
+          <button onClick={() => setColor("Black")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"black"}}>Black</button>
+          <button onClick={() => setColor("Blue")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"Blue"}}>Blue</button>
+          <button onClick={() => setColor("Yellow")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor:"Yellow"}}>Yellow</button>
+          <button onClick={() => setColor("Grey")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"Grey"}}>Grey</button>
+          <button onClick={() => setColor("White")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor:"white"}}>White</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
