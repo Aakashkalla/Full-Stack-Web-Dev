0f33231fae2a7949cbfef60d216b4c943a8d8512
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-08-05 20:00:37 +0530

    Basic Changes

--- Miniprojects/Currency Convertor/src/App.jsx
+++ Miniprojects/Currency Convertor/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
             <div className="relative w-full h-0.5">
               <button
                 type="button"
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-800 text-white px-2 py-0.5"
                 onClick={swap}
               >
                 swap
@@ -70,7 +70,7 @@ function App() {
             </div>
             <button
               type="submit"
-              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg cursor-pointer"
+              className="w-full bg-slate-800 text-white px-4 py-3 rounded-lg cursor-pointer"
             >
               Convert {from.toUpperCase()} to {to.toUpperCase()}
             </button>
