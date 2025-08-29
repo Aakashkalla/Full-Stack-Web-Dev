19635bf67330ae49beec8eefdc5a33fa4f2a3cdf
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-08-29 22:39:12 +0530

    Revised Basic JS

---
+++ JS Revisions/Day 1/variables.js
@@ -0,0 +1,31 @@
+const name = 'Aakash'
+var number = 892834
+let id = 234
+
+/* Prefer not to use var because of issue in functional and block scoping*/
+
+// console.table([name,number,id])
+
+//number => 2 to power 53
+//bigint
+//string =>""
+// boolean = true/false
+//null => standalone value
+// undefined => no value has been assigned
+// symbol=> unique 
+
+//object
+
+// console.log(typeof null) // ans is object
+// console.log(typeof undefined) // ans is undefined
+
+
+let str1 = "Hello"
+let str2 = " Aakash"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2)
+console.log("2" + 1)
+console.log("2" + 1 + 1)
+console.log(2 + 1 + "1")
