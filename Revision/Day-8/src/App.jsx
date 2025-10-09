d8655768561056c25273d24eb35782fe09d2fbc5
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-10-09 23:26:10 +0530

    Simple React Count Project

---
+++ Revision/Day-8/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card">
+        <p>Count : {count}</p>
+        <button onClick={() => setCount((count) => count + 1)}>
+          Increase Count
+        </button>
+        <button onClick={()=>setCount((count)=> count - 1)}>
+          Decrease Count
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
