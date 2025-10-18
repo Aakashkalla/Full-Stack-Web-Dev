64e7fe3e63c1a14c41b767d184a9a852d703881c
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-10-19 00:21:33 +0530

    Learned About Recoil

--- Revision/Day-12/react-recoil/src/App.jsx
+++ Revision/Day-12/react-recoil/src/App.jsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+import { counterAtom } from './store/counterAtom'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <RecoilRoot>
+        <Counter/>
+      </RecoilRoot>
     </>
   )
+}
+
+function Counter(){
+
+  return (
+    <>
+      <Count/>
+      <Increase/>
+      <Decrease/>
+    </>
+  )
+}
+
+function Count(){
+  const counter = useRecoilValue(counterAtom)
+  return (<h1>{counter}</h1>)
+}
+
+function Increase(){
+  const setCount = useSetRecoilState(counterAtom)
+  return <button onClick={()=>setCount(c=> c+1)}>Increase Count</button>
+} 
+
+function Decrease(){
+  const setCount = useSetRecoilState(counterAtom)
+  return <button onClick={()=>setCount(c=> c-1)}>Decrease Count</button>
 }
 
 export default App
