4745f7e1943297079ddd62d5b7cc1cd484e5d3e1
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-12-23 22:47:57 +0530

    Learned Nextjs

---
+++ Day-34-Nextjs/my-app/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>HELLOO FROM HOME PAGE</h1>
+  );
+}
---
+++ Day-34-Nextjs/my-app/app/user/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1 className="bg-amber-900 text-white">HELLO From user PAGE</h1>
+  );
+}
